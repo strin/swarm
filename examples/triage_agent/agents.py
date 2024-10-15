bdd30b0971109307ae88d0a03b503ea6533a06c1
Tim Shi <tim@cresta.ai>
2024-10-15 16:16:56 -0700

    Add testing code

--- examples/triage_agent/agents.py
+++ examples/triage_agent/agents.py
@@ -1,4 +1,5 @@
 from swarm import Agent
+from pprint import pprint
 
 
 def process_refund(item_id, reason="NOT SPECIFIED"):
@@ -30,17 +31,37 @@ refunds_agent = Agent(
 
 def transfer_back_to_triage():
     """Call this function if a user is asking about a topic that is not handled by the current agent."""
+    print("[mock] Transferring back to triage agent...")
     return triage_agent
 
 
 def transfer_to_sales():
+    print("[mock] Transferring to sales agent...")
     return sales_agent
 
 
 def transfer_to_refunds():
+    print("[mock] Transferring to refunds agent...")
     return refunds_agent
 
 
 triage_agent.functions = [transfer_to_sales, transfer_to_refunds]
 sales_agent.functions.append(transfer_back_to_triage)
 refunds_agent.functions.append(transfer_back_to_triage)
+
+
+if __name__ == "__main__":
+    from swarm import Swarm
+
+    client = Swarm()
+    response = client.run(
+        agent=triage_agent,
+        messages=[
+            {"role": "user", "content": "Hi!"},
+            {"role": "assistant", "content": "Hello, how can I help you today?"},
+            #{"role": "user", "content": "I want to buy some bees."},
+            {"role": "user", "content": "I want a refund for my order."},
+        ],
+        context_variables={"user_name": "John"},
+    )
+    pprint(response.messages)
